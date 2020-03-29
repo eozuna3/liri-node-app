9a5b670b4ee4c918d5420c04880e8a87476b22fe
eozuna3 <eo30239@gmail.com>
2020-03-29 12:19:59 -0500

    I was able to get the axios module call to the Bands In Town APi to work when given user input.

--- liri.js
+++ liri.js
@@ -2,4 +2,46 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+var moment = require("moment");
+
+//  "Concert-this" command section of the code
+var artist = "";
+for (let index = 2; index < process.argv.length; index++) {
+  if(artist === ""){
+    artist = process.argv[index];
+  } else {
+  artist = artist + " " + process.argv[index];
+  };
+};
+console.log(artist);
+var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+// The use of axios.get function in order to to grab data from the OMDB API and the Bands In Town API
+axios.get(concertURL).then(
+  function(response) {
+    // If the axios was successful...
+    // Then log the body from the site!
+    console.log(response.data);
+  },
+
+  function(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an object that comes back with details pertaining to the error that occurred.
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  }
+);
