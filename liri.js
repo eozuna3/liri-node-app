b854f4a039ef7519f5410a330149b063c11025a1
eozuna3 <eo30239@gmail.com>
2020-03-29 19:57:48 -0500

    I completed the addition of the event date to displaying in the console.

--- liri.js
+++ liri.js
@@ -10,7 +10,7 @@ var commands = process.argv[2].toLowerCase();
 
 switch (commands) {
   //  "Concert-this" command section of the code
-  case "concert-this":    
+  case "concert-this":
     var artist = "";
     for (let index = 3; index < process.argv.length; index++) {
       if (artist === "") {
@@ -19,21 +19,23 @@ switch (commands) {
         artist = artist + " " + process.argv[index];
       };
     };
-    console.log(artist);
+
+    //  Create URL to call the bands in town API
     var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-    // The use of axios.get function in order to to grab data from the OMDB API and the Bands In Town API
+    // The use of axios.get function in order to to grab data from the Bands In Town API
     axios.get(concertURL).then(
       function (response) {
         // If the axios was successful...
-        // Then log the body from the site!
-        console.log(response.data);
-        // console.log(response.data[0].venue.name);
-        // console.log(response.data[0].venue.city);
-        // console.log(response.data[0].venue.region);
-        for (let index = 0; index < response.data.length; index++) {
-          console.log(artist + " will be playing at " + response.data[index].venue.name + " which is located in " + response.data[index].venue.city + ", " + response.data[index].venue.region + ", " + response.data[index].venue.country + ".");
-        };        
+        if (response.data.length > 0) {
+          for (let index = 0; index < response.data.length; index++) {
+            var eventDate = moment(response.data[index].datetime);
+            console.log(artist + " will be playing at " + response.data[index].venue.name + ", which is located in " + response.data[index].venue.city + ", " + response.data[index].venue.region + ", " + response.data[index].venue.country + ".");
+            console.log("The concert is scheduled for " + eventDate.format("MM/DD/YYYY") + ".");
+          };
+        } else {
+          console.log("There were no event dates found for this artist.")
+        };
       },
       function (error) {
         if (error.response) {
@@ -54,7 +56,15 @@ switch (commands) {
       }
     );
     break;
-    
-    default:
-      console.log("Please enter a correct command for the program to use.");
+
+  //  "Movie-this" command section of the code
+  case "movie-this":
+      var movieTitle = "Mr Nobody";
+      var movieURL = "";
+
+      console.log(process.argv[3]);
+      break;
+
+  default:
+    console.log("Please enter a correct command for the program to use.");
 }
